e19500acf15b8c52d4e31933bc73d6d48f86ba50
Jeffrey Espiritu <jeff_pro3@hotmail.com>
2014-07-17 00:42:31 -0400

    Added some new CSS rules for table alignment and other things -Revamped downloads page with warning and overall better look

--- downloads.php
+++ downloads.php
@@ -6,22 +6,25 @@ include_once 'query.php'; // imports queries
 include_once 'functions.php';
 
 caption('Downloads');
-echo '<table class="maintable">
-		<tr>
-			<td>Client</td>
-			<td><a href="/data/TestRO.rar">TestRO Client</a></td>
-		</tr>
-		<tr>
-			<td>&nbsp</td>
-			<td>
-				<ol>
-					<h2>How to Install</h2>
-					<li>Extract the TestRO.rar file to your RO folder<br/><br/>
-					<span style="font-weight: bold">Note: You may copy the RO folder, though not required. You can simply
-					copy extract the contents of the archive straight to your RO folder</span></li>
-				<ol>
-			</td>
-	  </table>';
+echo '
+<div>
+	<p>Client Download: <span><a id="download" href="/data/TestRO.rar">TestRO Client</a></span></p>
+		<h2>How to Install</h2>
+		<p>There are two ways to install the RO client</p>
+	<ol>
+		<li>Copy your RO folder</li>
+		<li>Extract the TestRO.rar file to your RO folder</li>
+		<li>Run TestRO.exe</li>
+	</ol>
+	<p>OR</p>
+	<ol>
+		<li>Extract the TestRO.rar file directly your existing RO folder</li>
+		<p class="warning">Warning: This will overwrite two files common to most RO installations: 
+		<span class="file-underline">data.ini</span> and <span class="file-underline">data/clientinfo.xml</span><br/>
+		This is only recommended IF those two files are not needed or don\'t exist.<br/>
+		Otherwise this may break your existing RO installation.</p>
+	</ol>
+</div>';
 fim();
 
 ?>
