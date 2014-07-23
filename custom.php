855f8e4e790fc1401e8364a496d06bf533fe5911
Jeffrey Espiritu <jeff_pro3@hotmail.com>
2014-07-23 02:28:17 -0400

    Added page about server customizations with ItemDB entries -Items are grouped by category -Add CSS styles for the Item table to mirror Tro -Read description from idnum2item*.txt files

---
+++ custom.php
@@ -0,0 +1,124 @@
+<?php
+
+session_start();
+include_once 'config.php'; // loads config variables
+include_once 'query.php'; // imports queries
+include_once 'functions.php';
+
+$newitem = "[<span class='newitem'>NEW</span>]";
+
+$items = array(
+	"consume"=>array(
+		array("$newitem Guarana Candy", 12414),
+		array("Reduced weight of Grape Juice to 0.5", 533),
+		array("Increased the effectiveness of Piece of Cake", 539),
+		array("Reduced weight of Condensed White Potion to 1", 547),
+	),
+	"equipment"=>array(
+		array("$newitem Deviruchi Headphones", 18653),
+		array("$newitem Poker Card in Mouth", 18750),
+		array("$newitem Elven Bow[1], Elven Arrow, Elven Arrow Quiver", 1747, 1773, 20157),
+		array("Ice Pick, Ice Pick[1] does -30% less to Demi-Humans, Guardians, and Emperium", 1230, 13017),
+		array("Lord Kaho's Horns", 5013),
+		array("Pussy Cat Bell", 5051),
+	),
+	"cards"=>array(
+		array("Alligator Card", 4252),
+		array("Assassin Cross Card", 4359),
+		array("Bloody Knight Card", 4320),
+		array("Dark Illusion Card", 4169),
+		array("Dark Lord Card", 4168),
+		array("Doppelganger Card", 4142),
+		array("Dracula Card", 4134),
+		array("Eddga Card", 4123),
+		array("Evil Snake Lord Card", 4330),
+		array("Gargoyle Card", 4149),
+		array("General Egnigem Cenia Card", 4352),
+		array("Gloom Under Night Card", 4408),
+		array("Gold Queen Scaraba Card", 4509),
+		array("Gryphon Card", 4163),
+		array("High Priest Card", 4363),
+		array("High Wizard Card", 4365),
+		array("Ifrit Card", 4430),
+		array("Kiel D-01 Card", 4403),
+		array("Ktullanux Card", 4419),
+		array("Lady Tanee Card", 4376),
+		array("Lord Knight Card", 4357),
+		array("Lord of the Dead", 4276),
+		array("Maya Card", 4146),
+		array("Mysteltainn Card", 4207),
+		array("Queen Scaraba Card", 4507),
+		array("RSX-0806 Card", 4342),
+		array("Samurai Card", 4263),
+		array("Sniper Card", 4367),
+		array("Stem Worm Card", 4224),
+		array("Sting Card", 4226),
+		array("Stormy Knight Card", 4318),
+		array("Thanatos Card", 4399),
+		array("Toad Card", 4306),
+		array("Turtle General Card", 4305),
+		array("Valkyrie Randgris Card", 4407),
+		array("Vesper Card", 4374),
+		array("Whitesmith Card", 4361),
+
+	),
+);
+
+caption('Customizations');
+
+function expand($ids) {
+	$span = "
+	<span
+		class='link expand'
+		onClick=\"this.style.visibility='hidden'; $(this).siblings('span.hide').css('visibility', 'visible');
+			$(this).siblings('div').css('display', 'block');";
+	for ($x=1; $x<count($ids); $x++) {
+		$span .= "LINK_ajax('item.php?id=".$ids[$x]."', 'item_".$ids[$x]."');";
+	}
+	$span .= "
+	\">
+		&gt;&gt;
+	</span>";
+	return $span;
+}
+
+function hide($id=0) {
+	$span = "
+	<span
+		class='link hide'
+		onClick=\"this.style.visibility='hidden'; $(this).siblings('span.expand').css('visibility', 'visible');
+			$(this).siblings('div').css('display', 'none');\"
+	>
+		&lt;&lt;
+	</span>";
+	return $span;
+}
+
+function writegroup($h1, $group) {
+	echo "<h1>$h1</h1>";
+	
+	foreach ($group as $line) {
+		echo '<ul>
+			  <li>'.$line[0];
+		
+		echo expand($line) . hide();
+		
+		for ($x=1; $x<count($line); $x++) {
+			echo '<div class="item" id="item_'.$line[$x].'"></div>';
+		}
+		echo '</ul>';
+	}
+}
+
+echo '
+<div>';
+
+foreach ($items as $key=>$arr) {
+	writegroup($key, $arr);
+}
+
+echo '
+</div>';
+fim();
+
+?>
---
+++ item.php
@@ -0,0 +1,154 @@
+<?php
+
+session_start();
+include_once 'config.php'; // loads config variables
+include_once 'query.php'; // imports queries
+include_once 'functions.php';
+
+if (!isset($GET_id))
+	return;
+	
+$desc_file = "idnum2itemdesctable.txt";
+$name_file = "idnum2itemdisplaynametable.txt";
+
+$DESC = file_get_contents($desc_file);
+$NAME = file_get_contents($name_file);
+
+$desc = "";
+$matches = array();
+$desc_re = "/$GET_id#(.*?)\b[a-zA-Z ]+\s*:/s";
+
+$item_types = array("Healing", "Unknown", "Usable", "ETC", "Weapon", 
+					"Armor", "Card", "Pet Egg", "Pet Armor", "Unknown2",
+					"Ammo", "Delay Consume", "Cash");
+$equip_locations = array(1, 2, 4, 8, 16, 32, 64, 128, 256, 512);
+$equip_position = array("1"=>"Lower Headgear", 
+						"2"=>"Weapon", 
+						"4"=>"Garment", 
+						"8"=>"Accessory", 
+						"16"=>"Armor", 
+						"32"=>"Shield",
+						"34"=>"Two Handed",
+						"64"=>"Shoes", 
+						"128"=>"Accessory",
+						"136"=>"Accessory",
+						"256"=>"Upper Headgear", 
+						"257"=>"Upper and Lower",
+						"512"=>"Middle",
+						"513"=>"Middle and Lower",
+						"768"=>"Upper and Middle",
+						"769"=>"Upper, Middle, and Lower"
+					);
+					
+$weapon_types = array("Bare Hands", // 0
+					  "Dagger", // 1
+					  "One-Handed Sword", "Two-Handed Sword", "One-Handed Spear", "Two-Handed Spear",  // 2-5
+					  "One-Handed Axe", "Two-Handed Axe",  // 6, 7
+					  "Mace", "Two-Handed Mace",  // 8, 9
+					  "Staff",  // 10
+					  "Bow", // 11
+					  "Knuckle",  // 12
+					  "Musical Instrument",  // 13
+					  "Whip",  // 14
+					  "Book",  // 15
+					  "Katar",  // 16
+					  "Revolver", "Rifle", "Gatling", "Shotgun", "Grenade",  // 17-21
+					  "Huuma",  // 22
+					  "Two-Handed Staff"  // 23
+					);
+
+if ($DESC) {
+	// retrieve description for item
+	if (preg_match($desc_re, $DESC, $matches)) {
+		$desc = $matches[1];
+		// remove hidden underscore _
+		$desc = preg_replace('/\^[f]{6}_\^[0]{6}/', '',  $desc);
+		$desc = preg_replace('/^(\r\n|\n)*|(\r\n|\n)*$/', '', $desc);  // remove leading and trailing newlines
+		$desc = preg_replace('/\r\n|\n|$/', '<br/><br/>', $desc);  // replace newlines with <br/>
+		// replace color syntax with css
+		$desc = preg_replace('/\^([0-9a-fA-F]{6})(.*?)\^[0]{6}/', '<span style="color:#$1;">$2</span>', $desc);
+	}
+}
+
+$query = 'SELECT * FROM `item_db` WHERE `id` = ?';
+$stmt = prepare_query($query, 0, 'i', $GET_id);
+$result = execute_query($stmt, 'item.php');
+
+$line = $result->fetch_assoc();
+
+$slot = $line['slots']!==NULL?'['.$line['slots'].']':'';
+
+echo '
+<table class="item">
+	<tr>
+		<td class="top">&nbsp</td>
+		<td class="top" colspan="8">'.$line['name_japanese'].' '.$slot.' - ID #'.$line['id'].' - ('.$line['name_english'].')</td>
+		<td class="top">&nbsp</td>
+	<tr>
+	<tr>
+		<td class="attribute">Type:</td>
+		<td>'.$item_types[$line['type']].'</td>';
+		
+if ($line['type'] == 5 || $line['type'] == 6) {  // Armor or Card
+echo '
+		<td class="attribute">Location:</td>
+		<td>'.$equip_position[$line['equip_locations']].'</td>';
+} else if ($line['type'] == 4) {  // Weapon
+echo '
+		<td class="attribute">Class:</td>
+		<td>'.$weapon_types[$line['view']].'</td>';
+} else {  // Other
+echo '
+		<td class="attribute">&nbsp</td>
+		<td>&nbsp</td>';
+}
+echo '
+		<td class="attribute">Sell:</td>
+		<td>'.$line['price_sell'].'</td>
+		<td class="attribute">Buy:</td>
+		<td>'.$line['price_buy'].'</td>
+		<td class="attribute">Weight:</td>
+		<td>'.($line['weight']/10).'</td>
+	</tr>';
+if ($line['type'] == 4) {  // Weapon
+echo '
+	<tr>
+		<td class="attribute">Attack:</td>
+		<td>'.$line['attack'].'</td>
+		<td class="attribute">&nbsp</td>
+		<td>&nbsp</td>
+		<td class="attribute">Weapon Level:</td>
+		<td>'.$line['weapon_level'].'</td>
+		<td class="attribute">Required Level</td>
+		<td>'.$line['equip_level'].'</td>
+		<td class="attribute">&nbsp</td>
+		<td>&nbsp</td>
+	</tr>';
+} else if ($line['type'] == 5) {  // Armor
+echo '
+	<tr>
+		<td class="attribute">Defense:</td>
+		<td>'.$line['defence'].'</td>
+		<td class="attribute">Required Lvl:</td>
+		<td>'.$line['equip_level'].'</td>
+		<td class="attribute">&nbsp</td>
+		<td>&nbsp</td>
+		<td class="attribute">Slot:</td>
+		<td>'.$line['slots'].'</td>
+		<td class="attribute">&nbsp</td>
+		<td>&nbsp</td>
+	</tr>';
+}
+echo '
+	<tr>
+		<td class="attribute">Description:</td>
+		<td colspan="9">'.$desc.'</td>
+	</tr>
+	<tr>
+		<td class="attribute">Script:</td>
+		<td colspan="9">['.$line['script'].'], ['.$line['equip_script'].'], ['.$line['unequip_script'].']</td>
+	</tr>
+</table>';
+fim();
+
+?>
